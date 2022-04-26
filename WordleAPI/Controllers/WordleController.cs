6c8ec0e6fcb9dc7c6fcc394c88c1e344f8afad28
John Garrison <johngarrison1996@gmail.com>
2022-04-25 20:46:49 -0500

    Add project files.

---
+++ WordleAPI/Controllers/WordleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WordleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordleController : ControllerBase
+    {
+
+        string word = "SALET";
+        // GET: api/<WordleController>
+        [HttpGet]
+        public string Get()
+        {
+            return word;
+        }
+
+        // GET api/<WordleController>/5
+        [HttpGet("{guess}")]
+        public Wordle Get(string guess)
+        {
+            return new Wordle(word, guess);
+        }
+    }
+}
---
+++ WordleAPI/Wordle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+
+namespace WordleAPI
+{
+    public class Wordle
+    {
+        public string Guess { get; set; }
+        public bool Correct{get;set;}
+        public ArrayList curCorrect { get; set; }
+
+        public Wordle(string curWord, string guess)
+        {
+            this.Guess = guess;
+            if (curWord == guess)
+            {
+                this.curCorrect = new ArrayList() {2,2,2,2,2};
+                this.Correct = true;
+            }
+            else
+            {
+                this.curCorrect = new ArrayList();
+                HashSet<int> locs = new HashSet<int>();
+                for (int i =0; i < curWord.Length; i++)
+                {
+                    if (curWord[i] == guess[i])
+                    {
+                        this.curCorrect.Add(2);
+                        locs.Add(i);
+                    }
+                    else if ((curWord.IndexOf(guess[i]) >= 0)&&!locs.Contains(curWord.IndexOf(guess[i])))
+                    {
+                        this.curCorrect.Add(1);
+                        locs.Add(i);
+                    }
+                    else
+                    {
+                        this.curCorrect.Add(0);
+                    }
+                }
+                this.Correct=false;
+            }
+        }
+    }
+}
